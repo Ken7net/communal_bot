0f08f6bbe05e7645bd21e7654c759ed88acab272
Ken7net <105015354+Ken7net@users.noreply.github.com>
2025-11-27 14:41:43 +0300

    add migration

--- communal_bot/settings.py
+++ communal_bot/settings.py
@@ -7,6 +7,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'communal-bot.onrender.com']
 # if config('RENDER_EXTERNAL_HOST', default=''):
 #     ALLOWED_HOSTS.append(config('RENDER_EXTERNAL_HOST'))
@@ -32,6 +34,13 @@ DATABASES = {'default': {
 if config('DATABASE_URL', default=''):
     import dj_database_url
     DATABASES['default'] = dj_database_url.parse(config('DATABASE_URL'))
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 USE_TZ = True
 TIME_ZONE = 'UTC'
